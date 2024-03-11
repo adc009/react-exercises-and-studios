3a81d4dc88830f6c959a5c0bec0cd544dd38e29f
ShadowyBlue <alandchung09@gmail.com>
2024-03-11 17:59:40 -0500

    completed part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,8 +1,8 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I read";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/9/9d/Fullmetal123.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/615ZIxEDozL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/91zdHwFmpRL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
