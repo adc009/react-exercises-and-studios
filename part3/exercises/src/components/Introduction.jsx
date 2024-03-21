083efa7993e60fccbb4f6c405277c582662fdb17
ShadowyBlue <alandchung09@gmail.com>
2024-03-21 17:24:44 -0500

    completed exercises part 3

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Cooking</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Cooking makes food taste better.</li>
+            <li>Not all food has to be cooked.</li>
+            <li>Don't burn your food.</li>
          </ol>
       </div>
    );
